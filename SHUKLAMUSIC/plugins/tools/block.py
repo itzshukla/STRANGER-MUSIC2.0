eb5f6e69e874745ef7fc439e852bc158ea673260
Shashank Shukla <147219905+itzshukla@users.noreply.github.com>
2025-08-12 09:53:55 +0530

    Update block.py

--- SHUKLAMUSIC/plugins/tools/block.py
+++ SHUKLAMUSIC/plugins/tools/block.py
@@ -21,7 +21,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🎁 𝐃ᴏɴᴀᴛᴇ 🎁", url=f"https://t.me/SHIVANSH474")
+                        "🎁 𝐃ᴏɴᴀᴛᴇ 🎁", url=f"https://t.me/itszShukla")
                 ]
             ]
         ),
@@ -38,7 +38,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🎁 𝐃ᴏɴᴀᴛᴇ 🎁", url=f"https://t.me/SHIVANSH474")
+                        "🎁 𝐃ᴏɴᴀᴛᴇ 🎁", url=f"https://t.me/itszShukla")
                 ]
             ]
         ),
@@ -55,7 +55,7 @@ async def help(client: Client, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🎁 𝐃ᴏɴᴀᴛᴇ 🎁", url=f"https://t.me/SHIVANSH474")
+                        "🎁 𝐃ᴏɴᴀᴛᴇ 🎁", url=f"https://t.me/itszShukla")
                 ]
             ]
         ),
